8215f81974ec0a2398f675af668592593cf417d3
John Barry <john.olivierb@gmail.com>
2024-10-07 18:00:57 -0400

    Add schema for experience, research and software

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,5 +1,8 @@
 import { type SchemaTypeDefinition } from 'sanity'
+import research from './research'
+import software from './software'
+import experience from './experience'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [research, software, experience],
 }
---
+++ src/sanity/schemaTypes/research.ts
@@ -0,0 +1,20 @@
+import { defineField, defineType } from 'sanity'
+
+export default defineType({
+  name: 'research',
+  title: 'Research',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'content',
+      title: 'Content',
+      type: 'array',
+      of: [{ type: 'block' }]
+    }),
+  ],
+})
